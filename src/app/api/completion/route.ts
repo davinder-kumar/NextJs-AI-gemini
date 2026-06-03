1cf4be74e02271eabcd35f7f7786475d07cf7ae7
Davinder Kumar <davinderkumar8166@gmail.com>
2026-06-04 02:34:46 +0530

    Implement Completion and Stream text

--- src/app/api/completion/route.ts
+++ src/app/api/completion/route.ts
@@ -1,10 +1,11 @@
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
 
-export async function POST() {
+export async function POST(request: Request) {
+  const { prompt } = await request.json();
   const { text } = await generateText({
     model: google("gemini-2.5-flash"),
-    prompt: "Hi Gemini, Hello from Developer.",
+    prompt,
   });
 
   return Response.json({ text });
---
+++ src/app/api/stream/route.ts
@@ -0,0 +1,19 @@
+import { google } from "@ai-sdk/google";
+import { smoothStream, streamText } from "ai";
+
+export async function POST(request: Request) {
+  try {
+    const { prompt } = await request.json();
+    const text = streamText({
+      model: google("gemini-2.5-flash"),
+      prompt,
+      experimental_transform: smoothStream({
+        chunking: "word",
+      }),
+    });
+
+    return text.toUIMessageStreamResponse();
+  } catch (e) {
+    return new Response("Failed", { status: 500 });
+  }
+}
